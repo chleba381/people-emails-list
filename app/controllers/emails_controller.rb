ff8bddf1ed20a028edff422af14d332725d7b360
chleba381 <daniel-sykora@email.cz>
2022-02-21 01:06:30 +0100

    added pg to production

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -58,7 +58,7 @@ class EmailsController < ApplicationController
     @email.destroy
 
     respond_to do |format|
-      format.html { redirect_to emails_url, notice: "Email was successfully destroyed." }
+      format.html { redirect_to people_url, notice: "Email was successfully destroyed." }
       format.json { head :no_content }
     end
   end
